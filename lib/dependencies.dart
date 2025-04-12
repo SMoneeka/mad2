652b18e49e974dad382603d88d81ef026058e939
SMoneeka <115525155+SMoneeka@users.noreply.github.com>
2025-04-12 10:50:53 +0530

    Add files via upload

---
+++ lib/dependencies.dart
@@ -0,0 +1,31 @@
+dependencies:
+  flutter:
+    sdk: flutter
+
+  # To launch URLs like Google Maps
+  url_launcher: ^6.2.5
+
+  # To display animations
+  lottie: ^2.7.0
+
+  # For playing videos
+  video_player: ^2.7.1
+
+  # For handling time zones
+  timezone: ^0.9.2
+  intl: ^0.18.1
+
+  # For location and map-related features
+  google_maps_flutter: ^2.5.2
+  geolocator: ^11.0.0
+  google_places_flutter: ^2.0.6
+
+  dependencies:
+  flutter:
+    sdk: flutter
+  cupertino_icons: ^1.0.8
+  shared_preferences: ^2.2.2
+  fl_chart: ^0.64.0
+  google_fonts: ^6.1.0
+  url_launcher: ^6.2.5
+  
